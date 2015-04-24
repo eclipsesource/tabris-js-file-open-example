a391ce46e044d4c3ab05a18bd6a7cf7a527c86b2
Christian Petrov <cpetrov@eclipsesource.com>
2015-04-24 15:18:48 +0200

    Allow specifying file information by hand

--- www/app.js
+++ www/app.js
@@ -1,21 +1,40 @@
-tabris.create("Page", {title: "Tabris.js File Open Example", topLevel: true}).open();
+var page = tabris.create("Page", {title: "Tabris.js File Open Example", topLevel: true}).open();
 var fileTransfer = new FileTransfer();
-var uri = encodeURI("http://eclipsesource.com/fileadmin/images/es-logo.png");
 
-fileTransfer.download(
-  uri,
-  "cdvfile://localhost/temporary/es-logo.png",
-  function(entry) {
-    openFile(entry.toURL());
-  },
-  function(error) {
-    console.log("download error source " + error.source);
-    console.log("download error target " + error.target);
-    console.log("upload error code" + error.code);
-  },
-  false
-);
+var urlInput = tabris.create("TextInput", {
+  message: "URL",
+  layoutData: {left: "10%", top: "10%", right: "10%"}
+}).appendTo(page);
+
+var mimeTypeInput = tabris.create("TextInput", {
+  message: "Mime type",
+  layoutData: {left: "10%", top: [urlInput, 15], right: "10%"}
+}).appendTo(page);
+
+var localFileName = tabris.create("TextInput", {
+  message: "Local file name",
+  layoutData: {left: "10%", top: [mimeTypeInput, 15], right: "10%"}
+}).appendTo(page);
+
+tabris.create("Button", {
+  text: "Download",
+  layoutData: {top: [mimeTypeInput, 15], centerX: 0}
+}).appendTo(page).on("select", function() {
+  fileTransfer.download(
+    urlInput.get("text"),
+    "cdvfile://localhost/temporary/" + localFileName.get("text"),
+    function(entry) {
+      openFile(entry.toURL());
+    },
+    function(error) {
+      console.log("download error source " + error.source);
+      console.log("download error target " + error.target);
+      console.log("upload error code" + error.code);
+    },
+    false
+  );
+});
 
 function openFile(localPath) {
-  cordova.plugins.fileOpener2.open(localPath, "image/png");
+  cordova.plugins.fileOpener2.open(localPath, mimeTypeInput.get("text"));
 }
